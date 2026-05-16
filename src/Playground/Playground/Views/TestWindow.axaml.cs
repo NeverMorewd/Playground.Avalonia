1ffc1c141fcf4698823df4222326215b46fdd926
Easley <nevermore.wd@me.com>
2026-05-16 17:27:31 +0200

    add testwindow for  BorderOnly issue

---
+++ src/Playground/Playground/Views/TestWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Playground.Views;
+
+public partial class TestWindow : Window
+{
+    public TestWindow()
+    {
+        InitializeComponent();
+    }
+}
